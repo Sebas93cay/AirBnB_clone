aad2f5801a8be087c8ad5f12f90fceeb856c539c
Seb Asus VS <sebas93cay@gmail.com>
2021-06-25 13:56:00 -0500

    Update __init__ in baseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,18 +4,26 @@ This script contains the base model
 """
 
 import uuid
-import datetime
+#import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """Base class to all other objects in the H_Air_BNB project"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Init Base function"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
+        if kwargs == {}:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    setattr(self, k, v)
+            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+                
     def __str__(self) -> str:
         """str function"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
