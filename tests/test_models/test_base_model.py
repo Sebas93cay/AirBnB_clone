6560921f71b1f2c3a6af9baf2e7187b9408471f0
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-26 15:55:20 +0000

    delete cache and add test, 2 test failed in iso format

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """This module has the test for the base class"""
-
+import io
+from contextlib import redirect_stdout
 from models.base_model import BaseModel
+from datetime import datetime
 import unittest
 
 
-class testBase(unittest.TestCase):
+class testBaseModel(unittest.TestCase):
     """test for base class"""
 
-    def test_dates(self):
+    maxDiff = None
+
+    def test_base_model_save(self):
         """test created_at and updated_at instance variables"""
         b1 = BaseModel()
         created1 = b1.created_at
@@ -16,3 +20,54 @@ class testBase(unittest.TestCase):
         b1.save()
         self.assertEqual(created1, b1.created_at)
         self.assertNotEqual(uptaded1, b1.updated_at)
+        self.assertIsInstance(b1.created_at, datetime)
+        self.assertIsInstance(b1.updated_at, datetime)
+
+    def test_base_model_save_parameters(self):
+        b1 = BaseModel()
+
+        with self.assertRaises(TypeError):
+            b1.save(212)
+            b1.save("str")
+            b1.save({})
+
+    def test_base_model_ids(self):
+        """test id instance variable"""
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+        self.assertIsInstance(b1.id, str)
+
+    def test_base_model_str(self):
+        b1 = BaseModel()
+        b1.name = "Holberton"
+        b1.my_number = 89
+        r = "[{}] ({}) {}\n".format(type(b1).__name__, b1.id, b1.__dict__)
+
+        with redirect_stdout(io.StringIO()) as f:
+            print(b1)
+        self.assertEqual(f.getvalue(), r)
+
+    def test_base_model_to_dict(self):
+        b1 = BaseModel()
+        r = {"__class__": "BaseModel",
+             "id": b1.id,
+             "created_at": b1.created_at.isoformat(),
+             "updated_at": b1.updated_at.isoformat() }
+        self.assertDictEqual(b1.to_dict(), r)
+
+    def test_base_model_to_dict_parameters(self):
+        b1 = BaseModel()
+
+        with self.assertRaises(TypeError):
+            b1.to_dict(212)
+            b1.to_dict("str")
+            b1.to_dict({})
+
+    def test_base_model_init_copy(self):
+        b1 = BaseModel()
+        b2 = BaseModel(**b1.to_dict())
+
+        self.assertDictEqual(b1.to_dict(), b2.to_dict())
+        self.assertIsNot(b1, b2)
+
