72cee337a35c710934026baedc0c51855aef885a
Seb Asus VS <sebas93cay@gmail.com>
2021-06-26 00:52:22 -0500

    Create save to file

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,8 @@ This script contains the base model
 """
 
 import uuid
-#import datetime
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -17,20 +17,27 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             for k, v in kwargs.items():
                 if k != '__class__':
                     setattr(self, k, v)
-            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
-                
+            self.created_at = datetime.strptime(
+                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(
+                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+
     def __str__(self) -> str:
         """str function"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """update the updated_at instance variable to current time"""
-        self.updated_at = datetime.datetime.now()
+        """
+        Update the updated_at instance variable to current time
+        Save the instance into a json file
+        """
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This script contains the FileStorage class"""
+
+import json
+
+
+class FileStorage:
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects[type(obj).__name__+'.'+str(obj.id)] = obj
+
+    def reload(self):
+        from models.base_model import BaseModel
+        try:
+            with open(FileStorage.__file_path, mode="r", encoding="utf-8") as file:
+                tmp_dir = json.load(file)
+            for k, v in tmp_dir.items():
+                self.__objects[k] = BaseModel(**v)
+        except:
+            content = None
+
+    def save(self):
+        eldir = {}
+        for k in self.__objects.keys():
+            eldir[k] = self.__objects[k].to_dict()
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as file:
+            json.dump(eldir, file)
