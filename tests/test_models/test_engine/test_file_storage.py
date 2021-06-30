8c278a96ee813662081c638b2cfcbbce96509d90
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-30 19:20:13 +0000

    test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -17,21 +17,24 @@ from models.review import Review
 class TestFileStorage_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the FileStorage class."""
 
-
     def setUp(self):
+        """Delete file.json and set __objects = {} before each test"""
         if os.path.isfile('file.json'):
             os.remove('file.json')
         FileStorage._FileStorage__objects = {}
 
     def tearDown(self):
+        """Delete file.json and set __objects = {} after each test"""
         if os.path.isfile('file.json'):
             os.remove('file.json')
         FileStorage._FileStorage__objects = {}
 
     def test_fileStorage_from_models(self):
+        """test inicialization of storage var in models"""
         self.assertEqual(type(models.storage), FileStorage)
 
     def test_fileStorage_init_args(self):
+        """test constructor of FileStorage class"""
         with self.assertRaises(TypeError):
             FileStorage("12345")
             FileStorage(1234)
@@ -40,17 +43,23 @@ class TestFileStorage_instantiation(unittest.TestCase):
             FileStorage(None)
 
     def test_fileStorage_attrs(self):
+        """check the attributes of FileStorage class"""
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
         with self.assertRaises(AttributeError):
-            models.storage.file_path
-            models.storage.objects
+            models.storage.__file_path
+            models.storage.__objects
 
     def test_fileStorage_all(self):
+        """test normal use of function all """
         self.assertEqual(type(models.storage.all()), dict)
         self.assertEqual(len(models.storage.all()), 0)
         models.storage.new(BaseModel())
         self.assertEqual(len(models.storage.all()), 1)
 
     def test_fileStorage_all_args(self):
+        """test arguments of function all"""
         with self.assertRaises(TypeError):
             models.storage.all([])
             models.storage.all({})
@@ -59,7 +68,7 @@ class TestFileStorage_instantiation(unittest.TestCase):
             models.storage.all(None)
 
     def test_fileStorage_new(self):
-
+        """test normal use of function new"""
         b1 = BaseModel()
         u1 = User()
         s1 = State()
@@ -83,6 +92,7 @@ class TestFileStorage_instantiation(unittest.TestCase):
         self.assertIn("Review." + r1.id, models.storage.all().keys())
 
     def test_fileStorage_new_wrong_args(self):
+        """test function new without invalid arguments"""
         with self.assertRaises(AttributeError):
             models.storage.new([])
             models.storage.new({})
@@ -90,17 +100,44 @@ class TestFileStorage_instantiation(unittest.TestCase):
             models.storage.new(1)
             models.storage.new(None)
 
-
     def test_fileStorage_new_no_args(self):
+        """test function new without arguments"""
         with self.assertRaises(TypeError):
             models.storage.new()
 
     def test_fileStorage_save(self):
+        """test normal use of function save"""
         p = Place()
         models.storage.new(p)
         models.storage.save()
 
-        objs = ""
-
         with open("file.json", "r") as f:
             self.assertIn(p.id, f.read())
+
+    def test_fileStorage_save_args(self):
+        """test arguments of function save"""
+        with self.assertRaises(TypeError):
+            models.storage.save([])
+            models.storage.save(User())
+            models.storage.save({})
+            models.storage.save("")
+
+    def test_fileStorage_reload(self):
+        """test correct use of reload function"""
+        models.storage.reload()
+        self.assertEqual(os.path.isfile('file.json'), False)
+        self.assertEqual(models.storage.all(), {})
+
+        u = User()
+        models.storage.new(u)
+        models.storage.save()
+        FileStorage._FileStorage__objects = {}
+        models.storage.reload()
+
+        self.assertIn("User." + u.id, models.storage.all().keys())
+
+    def test_fileStorage_reload_args(self):
+        """test arguments of function reload"""
+        with self.assertRaises(TypeError):
+            models.storage.reload(None)
+            models.storage.reload([])
